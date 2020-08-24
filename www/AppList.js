f9aefcee74dc65b9e19f42c0c961a82328144564
AAnish11 <anish.agarwal2010@gmail.com>
2020-08-24 23:43:07 +0530

    Update AppList.js
    [feature] added feature for search music

--- www/AppList.js
+++ www/AppList.js
@@ -2,6 +2,9 @@ var AppList = {
 	applist : function (success, error) {
 		cordova.exec(success, error, 'AppList', 'applist', []);
 	},
+	appmusic : function (success, error) {
+		cordova.exec(success, error, 'AppList', 'scanmusic', []);
+	},
 	appicon: function (pkg, success, error) {
 		cordova.exec(success, error, 'AppList', 'appicon', [pkg]);
 	},
